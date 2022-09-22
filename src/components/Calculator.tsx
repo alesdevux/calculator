9d892763a8b9d6ee5145171a91db6e6a4e15fbec
Ales <aleswebgit@gmail.com>
2022-09-22 12:59:21 +0200

    add styles

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { evaluate } from 'mathjs'
+import './Calculator.css'
 
 export const rows: (number | string)[][] = [
   [7, 8, 9, '/'],
@@ -13,30 +14,33 @@ const Calculator = () => {
   const [value, setValue] = useState<Value>('')
 
   const createHandlerCell = (cell: Value) => {
-    const signEqual = cell === '='
+    const signEqual: boolean = cell === '='
     signEqual ? setValue(evaluate(value)) : setValue(value + cell)
   }
 
   return (
-    <div>
+    <>
       <h1>Calculator</h1>
-      <input readOnly value={value} />
-      <div role='grid'>
-        {rows.map((row, rowIndex) => (
-          <div key={rowIndex} role='row'>
-            {row.map(cell => (
-              <button
-                onClick={() => createHandlerCell(cell.toString())}
-                key={cell}
-                role='cell'
-              >
-                {cell}
-              </button>
-            ))}
-          </div>
-        ))}
+      <div className="calculator">
+        <input readOnly value={value} />
+        <div role='grid' className='buttons'>
+          {rows.map((row, rowIndex) => (
+            <div key={rowIndex} role='row'>
+              {row.map(cell => (
+                <button
+                  onClick={() => createHandlerCell(cell.toString())}
+                  key={cell}
+                  role='cell'
+                  className={cell === '=' ? 'equal' : ''}
+                >
+                  {cell}
+                </button>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 export default Calculator
